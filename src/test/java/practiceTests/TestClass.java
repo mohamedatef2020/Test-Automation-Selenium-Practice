6371533376502a94dbdca45cd19383f0e861ba08
mohamedatef2020 <mohamed.atef.pd2020@gmail.com>
2022-01-21 20:48:58 +0200

    first test

--- src/test/java/practiceTests/TestClass.java
+++ src/test/java/practiceTests/TestClass.java
@@ -1,4 +1,19 @@
 package practiceTests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TestClass {
+    @Test
+    public void firstTest(){
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.google.com/ncr");
+        Assert.assertEquals(driver.getTitle(), "Google");
+    }
 }
